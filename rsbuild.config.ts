e43077bff53d83e79770e1ba015c6bfcbe6291d7
maxjuniman <max.batistel@fastcard.onmicrosoft.com>
2026-02-02 17:04:34 -0300

    melhorias

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,4 +5,8 @@ import { pluginStyledComponents } from '@rsbuild/plugin-styled-components';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact(), pluginStyledComponents()],
+  html: {
+    title: 'Sensedia - Lista de Usuários',
+    favicon: './public/favicon.ico',
+  },
 });
